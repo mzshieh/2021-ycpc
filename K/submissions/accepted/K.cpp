66564d884fed327c49afcbc42a2be1b49a7251bb
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2021-09-08 10:01:41 +0800

    fix K sample solution

--- K/submissions/accepted/K.cpp
+++ K/submissions/accepted/K.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include<algorithm>
+#include<stdio.h>
 using namespace std;
 long long x[2000005],y[2000005];
 int main(){
@@ -12,10 +13,17 @@ int main(){
     }
     sort(x,x+n);
     sort(y,y+n);
-    long long dis=0;
-    for(int i = 0;i<n;i++){
-        dis+=abs(x[n/2]-x[i]);
-        dis+=abs(y[n/2]-y[i]);
+    long long dis=9e18;
+    for(int xx = x[n/2]-1;xx<=x[n/2]+1;xx++){
+        for(int yy = y[n/2]-1;yy<=y[n/2]+1;yy++){
+            if(abs(xx%2)!=abs(yy%2))continue;
+ 	    long long sum=0;
+            for(int i = 0;i<n;i++){
+                sum+=abs(xx-x[i]);
+                sum+=abs(yy-y[i]);
+            }
+            dis=min(dis, sum);
+        }
     }
     printf("%lld\n",dis/2);
 }
